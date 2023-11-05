c93000950a94a83a6951620cbb8489061dbe6a9f
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-11-05 19:39:02 +0530

    Update script.js

--- Magical Navigation Bar/script.js
+++ Magical Navigation Bar/script.js
@@ -1,1 +1,8 @@
+console.clear();
+const list = document.querySelectorAll('.list');
+const nav = document.querySelector('.navigation');
+list.forEach(item => item.addEventListener('click', function(e){
+	list.forEach(li => li.classList.remove('active'));
+	e.currentTarget.classList.add('active');
+}));  
 

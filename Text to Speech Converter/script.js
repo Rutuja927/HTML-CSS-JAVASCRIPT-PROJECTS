5cc9a1ea38d72e0cb07d63d321f413128198b805
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-11-18 19:11:19 +0530

    Update script.js

--- Text to Speech Converter/script.js
+++ Text to Speech Converter/script.js
@@ -1,1 +1,9 @@
-
+       function speak() {
+            const utter = new SpeechSynthesisUtterance();
+            utter.rate = 1.0; // speed: 0.1-10
+            utter.volume = 1.0; // 0-1
+            utter.pitch = 0.5; // 0 (low) - 2 (high)
+            utter.voice = speechSynthesis.getVoices()[0];
+            utter.text = document.querySelector('#txt').value;
+            speechSynthesis.speak(utter);
+        }

19be57df0d9d672b7843d0fa7776159dd5671b97
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-11-28 16:01:50 +0530

    Update script.js

--- Image Effect Hover on Name/script.js
+++ Image Effect Hover on Name/script.js
@@ -1,1 +1,20 @@
+console.clear();
+const elApp = document.querySelector("#app");
 
+document.body.addEventListener("mousemove", (event) => {
+  console.log({
+    x: event.clientX,
+    y: event.clientY
+  });
+
+  elApp.style.setProperty("--x", event.clientX);
+  elApp.style.setProperty("--y", event.clientY);
+});
+
+let el = document.body;
+el.addEventListener("click", function (e) {
+  el.hidden = true;
+  requestAnimationFrame(() => {
+    el.hidden = false;
+  });
+});

57bed0ee92fe7995b76ba5933ad3edd383270e39
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-12-01 18:33:44 +0530

    Update script.js

--- Submit Button Animation/script.js
+++ Submit Button Animation/script.js
@@ -1,1 +1,55 @@
 
+      var button = document.getElementById("js-btn"),
+        timer = document.getElementById("js-timer"),
+        reset = document.getElementById("js-reset");
+
+      button.addEventListener("click", doSubmit);
+      reset.addEventListener("click", resetButton);
+
+      function doSubmit() {
+        if (button.classList.contains("do-submit")) {
+          return;
+        }
+
+        button.classList.add("do-submit");
+
+        setTimeout(function () {
+          doTimer(0);
+        }, 1200);
+
+        setTimeout(function () {
+          doTimer(15);
+        }, 1200);
+
+        setTimeout(function () {
+          doTimer(75);
+        }, 2000);
+
+        setTimeout(function () {
+          doTimer(100);
+        }, 2800);
+      }
+
+      function doTimer(amountLoaded) {
+        timer.style.strokeDashoffset = 3.83 * (100 - amountLoaded) + "px";
+
+        if (amountLoaded === 100) {
+          setTimeout(function () {
+            button.classList.add("success");
+          }, 500);
+        }
+      }
+
+      function resetButton() {
+        button.classList.add("reset");
+        setTimeout(function () {
+          button.classList.remove("success");
+          button.classList.remove("do-submit");
+          button.classList.remove("reset");
+        }, 500);
+
+        timer.style.strokeDashoffset = "383px";
+
+        time = 90;
+      }
+    

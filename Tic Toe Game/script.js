399558a4c7e20d01a0dd7cee9ba7bc4154e862f1
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-12-01 15:52:35 +0530

    Update script.js

--- Tic Toe Game/script.js
+++ Tic Toe Game/script.js
@@ -1,1 +1,101 @@
+let btnRef = document.querySelectorAll(".button-option");
+let popupRef = document.querySelector(".popup");
+let newgameBtn = document.getElementById("new-game");
+let restartBtn = document.getElementById("restart");
+let msgRef = document.getElementById("message");
+
+let winningPattern = [
+  [0, 1, 2],
+  [0, 3, 6],
+  [2, 5, 8],
+  [6, 7, 8],
+  [3, 4, 5],
+  [1, 4, 7],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+let xTurn = true;
+let count = 0;
+
+const disableButtons = () => {
+  btnRef.forEach((element) => (element.disabled = true));
+
+  popupRef.classList.remove("hide");
+};
+
+const enableButtons = () => {
+  btnRef.forEach((element) => {
+    element.innerText = "";
+    element.disabled = false;
+  });
+
+  popupRef.classList.add("hide");
+};
+
+const winFunction = (letter) => {
+  disableButtons();
+  if (letter == "X") {
+    msgRef.innerHTML = "  <br> '1st player X ' Wins";
+  } else {
+    msgRef.innerHTML = "  <br> '2st player O' Wins";
+  }
+};
+
+const drawFunction = () => {
+  disableButtons();
+  msgRef.innerHTML = " <br> It's a Draw";
+};
+
+newgameBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+restartBtn.addEventListener("click", () => {
+  count = 0;
+  enableButtons();
+});
+
+const winChecker = () => {
+  for (let i of winningPattern) {
+    let [element1, element2, element3] = [
+      btnRef[i[0]].innerText,
+      btnRef[i[1]].innerText,
+      btnRef[i[2]].innerText,
+    ];
+    //Check if elements are filled
+    //If 3 empty elements are same and would give win as would
+    if (element1 != "" && (element2 != "") & (element3 != "")) {
+      if (element1 == element2 && element2 == element3) {
+        //If all 3 buttons have same values then pass the value to winFunction
+        winFunction(element1);
+      }
+    }
+  }
+};
+
+btnRef.forEach((element) => {
+  element.addEventListener("click", () => {
+    if (xTurn) {
+      xTurn = false;
+
+      element.innerText = "X";
+      element.disabled = true;
+    } else {
+      xTurn = true;
+
+      element.innerText = "O";
+      element.disabled = true;
+    }
+
+    count += 1;
+    if (count == 9) {
+      drawFunction();
+    }
+
+    winChecker();
+  });
+});
+
+window.onload = enableButtons;
 

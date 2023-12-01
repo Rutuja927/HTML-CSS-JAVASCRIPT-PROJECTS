a3e680005602e8417e2de6213fa239c9d7cde641
Rutuja Shinde <105160525+Rutuja927@users.noreply.github.com>
2023-12-01 09:01:42 +0530

    Update script.js

--- Animated Search Box/script.js
+++ Animated Search Box/script.js
@@ -1,1 +1,16 @@
-
+(function($){
+    var search_button = $('.fa-search'),
+        close_button  = $('.close'),
+        input = $('.input');
+    search_button.on('click',function(){
+      $(this).parent().addClass('open');
+      close_button.fadeIn(500);
+      input.fadeIn(500);
+    });
+    
+    close_button.on('click',function(){
+      search_button.parent().removeClass('open');
+      close_button.fadeOut(500);
+      input.fadeOut(500);
+    });
+  })(jQuery);
